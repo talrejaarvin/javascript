08995e738cdd378c590cd3e5d9150419ebebb4b7
talrejaarvin <talrejaarvin@gmail.com>
2024-03-16 18:03:22 +0500

    update datatypes.js

---
+++ 01_basics/02_datatyoes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // its is use for popup message but in browers not in Node;
+
+// datatypes
+// numbers
+// bigint
+//string = ""
+// boolean = true/false
+//null = standalone value / is a object 
+//undefined = value not defined
+// symbol = for uniqueness
+
+// object
