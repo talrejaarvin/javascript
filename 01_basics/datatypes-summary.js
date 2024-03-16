9b7e43c283d9080e13037919ff684d933b18adc8
talrejaarvin <talrejaarvin@gmail.com>
2024-03-16 20:54:11 +0500

    add datatypes-summary.js

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// 2 types of datatypes
+
+// 1. primitive
+
+// 7 types:
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+// 2. Refernce (Non Primitive)
+// 3 types of reference
+// 1 Arrays
+// 2 Object
+// 3 Function
+
+// declartion of Array
+
+const heros = ["shaktiman", "naagraj", "mumtaz molai"];
+console.log(heros);
+
+// declartion of objects
+
+const myObj = {
+  name: "arvin",
+  age: "22",
+};
+
+console.log(myObj);
+
+// declartion of functions
+
+const myFun = function () {
+  console.log("hello world");
+}
+
+console.log(myFun);
