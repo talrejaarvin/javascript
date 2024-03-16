674ac9f4a9f1042805e598a14fac14f3c69bb231
talrejaarvin <talrejaarvin@gmail.com>
2024-03-16 18:26:24 +0500

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score = "33";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1; false => 0;
+
