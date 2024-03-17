4c10a6e94620fa33fb119a002e28111c0fee8e75
talrejaarvin <talrejaarvin@gmail.com>
2024-03-17 20:48:55 +0500

    practice till nums and maths done

---
+++ Practice/01_practice.js
@@ -0,0 +1,66 @@
+// ************************ Variables and Data Types ***********************8
+
+// 1. Declare a variable named city and assign it the value "New York".
+const city = "New York";
+
+// 2. Create a variable called isLearning and assign it a boolean value to indicate that you are learning JavaScript.
+
+const isLearning = true;
+
+// ************************ Strings ******************************************
+
+// 1. How would you find the length of the string "Hello, world" in JavaScript?
+
+console.log("hello, world".length);
+
+// 2. Concatenate two strings "Hello" and "World" with a space in between and store them in a variable named greeting.
+
+const greeting = "Hello " + "World";
+console.log(greeting);
+
+// 3. Given a variable str with the value "JavaScript", how would you get the substring "Script" from it?
+
+let str = "JavaScript";
+console.log(str.substring(4, 10));
+
+// ***************************** Comparisons ******************************************
+
+// 1. Compare two strings "apple" and "Apple" to determine if they are equal.
+
+const var1 = "apple";
+const var2 = "Apple";
+console.log(var1 == var2);
+
+// 2. Check if the number stored in a variable num is less than 10. If so, print "Less than 10"; otherwise, print "10 or more".
+
+const num = 5;
+
+if (num < 10) {
+  console.log("less than 10");
+} else {
+  console.log("10 or more");
+}
+
+// ***************************** Conversions ********************************************
+
+// 1. Convert the string "123" into a number.
+
+const str1 = "123";
+console.log(Number(str1));
+
+// ************************** Numbers and Math **************************************8
+
+// 1. How do you round the number 7.25 to the nearest integer?
+
+console.log(Math.round(7.25));
+
+// 2. Calculate the remainder of 10 divided by 3 using a JavaScript operator.
+
+console.log(10 % 3);
+
+
+// 3. Using Math.pow(), calculate 2 raised to the power of 5
+
+console.log(Math.pow(2, 5));
+
+// practice done till maths and numbers
