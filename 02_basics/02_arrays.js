5dbf15712c33b88e93e5d8437e64f1b6322aec1b
talrejaarvin <talrejaarvin@gmail.com>
2024-03-20 17:07:15 +0500

    update 02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -13,8 +13,21 @@ const allHeros = [...marvel_heros, ...dc_heros]; // use this for combine
 // console.log(allHeros);
 
 const array1 = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
-console.log(array1);
+// console.log(array1);
 const fixArray1 = array1.flat(Infinity);
-console.log(fixArray1);
+// console.log(fixArray1);
 
+// console.log(Array.isArray("Arvin")); // for check the input is array or not
 
+// Array.from
+// console.log(Array.from("Arvin"));
+// console.log(Array.from({name: Arvin})); // intresting for interviews
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1, score2, score3));
+
+function createArray(...elements) {
+  return Array.of(...elements);
+}
